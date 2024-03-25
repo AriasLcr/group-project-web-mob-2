faecfc66580e679e09332e90fdaf0d5e227457e7
isbahsohaib <111761305+isbahsohaib@users.noreply.github.com>
2024-03-25 14:34:18 -0400

    creating header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
+    <title>Unix Survival Guide - Home</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <script src="assets/js/script.js"></script>
+</head>
+<body>
+
+<div id="sidebar">
+    <img id="logo" src="./assets/images/logo.png" alt="Group logo">
+    <h2>Table of Contents</h2>
+    <a href="./index.html" class="active">Introduction</a>
+    <a href="./history.html">History</a>
+    <a href="./terminal.html">Terminal</a>
+    <a href="./commands.html">Commands</a>
+    <a href="./practiceLessons.html">Practice Lessons</a>
+    <a href="./quizzes.html" class="active">Quizzes</a>
+</div>
